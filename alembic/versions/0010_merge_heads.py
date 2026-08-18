7fad0be36769114d07ad95c0e42800d66134888e
nafiz12s <142015206+nafizahmed12@users.noreply.github.com>
2026-08-18 16:50:36 +0600

    fix: update merge migration after shortening revision id

--- alembic/versions/0010_merge_heads.py
+++ alembic/versions/0010_merge_heads.py
@@ -1,13 +1,13 @@
 """Merge the supplier-fulfillment and digital/affiliate migration heads.
 
 Revision ID: 0010_merge_heads
-Revises: 0008_auto_supplier_fulfillment_on_payment, 0009_restore_stock
+Revises: 0008_supplier_payment_auto, 0009_restore_stock
 """
 from typing import Sequence, Union
 
 revision: str = "0010_merge_heads"
 down_revision: Union[str, Sequence[str], None] = (
-    "0008_auto_supplier_fulfillment_on_payment",
+    "0008_supplier_payment_auto",
     "0009_restore_stock",
 )
 branch_labels: Union[str, Sequence[str], None] = None
